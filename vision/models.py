ad202fde56146b36a7e4d6436689f7363a36f5f3
SAD LIFE <indeedintrovert@gmail.com>
2026-06-27 23:25:45 +0530

    Added models of the projects

--- vision/models.py
+++ vision/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+class Gesture(models.Model):
+    name = models.CharField(max_length=100,unique=True)
+    gesture_type=models.CharField(
+        max_length=20,
+        choices=[
+            ("static","Static"),
+            ("dynamic","Dynamic")
+        ],
+        default="static"
+    )
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
+
+class DatasetSample(models.Model):
+    gesture = models.ForeignKey(Gesture,on_delete=models.CASCADE)
+    landmark=models.JSONField()
+    image=models.ImageField(upload_to="samples/",null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.gesture.name} sample"
+class PredictionHistory(models.Model):
+    predicted_gesture = models.CharField(max_length=100)
+    confidence = models.FloatField()
+    landmark = models.JSONField(null=True,blank=True)
+    created_at=models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return f"{self.predicted_gesture}-{self.confidence}"
