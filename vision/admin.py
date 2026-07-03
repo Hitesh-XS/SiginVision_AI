c2227533c9d44d99e79e8448010d6f6681734f30
Hitesh_G <indeedintrovert@gmail.com>
2026-07-03 17:00:03 +0530

    Remove comment about model registration

--- vision/admin.py
+++ vision/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+
 from .models import Gesture,DatasetSample,PredictionHistory,DynamicDatasetSample,SentenceHistory
 
 admin.site.register(Gesture)
