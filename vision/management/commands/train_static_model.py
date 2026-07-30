60569538a6e1294ebec610fd47dd8a3df38096e8
Hitesh_G <indeedintrovert@gmail.com>
2026-07-30 17:54:04 +0530

    Document training pipeline in train_static_model.py
    Added a detailed pipeline comment outlining the steps for training a static model.

--- vision/management/commands/train_static_model.py
+++ vision/management/commands/train_static_model.py
@@ -1,5 +1,30 @@
+''''
+Pipline of this static train
+Read command options
+        ↓
+Load samples from database
+        ↓
+Validate 63 landmark values
+        ↓
+Normalize landmarks
+        ↓
+Encode gesture names
+        ↓
+Split train and test data
+        ↓
+Build dense neural network
+        ↓
+Train using training and validation data
+        ↓
+Evaluate using test data
+        ↓
+Save model, labels, and report
+
+'''
+
+
 import json
-import os
+
 from pathlib import Path
 
 import numpy as np
